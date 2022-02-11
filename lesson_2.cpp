c234e8ba7837e432f09b53cbe22506cf60ab0a02
TerentevVSs <terentev.vs@phystech.edu>
2022-02-11 10:14:48 +0300

    lesson 2: list.splice(), list.sort(), greater<>(), merge() less<>()

--- lesson_2.cpp
+++ lesson_2.cpp
@@ -1,3 +1,5 @@
+#include <list>
+#include <forward_list>
 #include <fstream>
 #include <random>
 #include <algorithm>
@@ -20,6 +22,21 @@ using namespace std;
 
 
 int main(){
+    list<int> numbers = {1,3,2,42 -325};
+    list<int> other_numbers = {25, -1, 30};
+    numbers.splice(next(numbers.begin()),
+                   other_numbers,
+                   other_numbers.begin(),
+                   prev(other_numbers.end()));
+    numbers.sort(less<>());
+    for(auto item: numbers){
+        cout << item << ' ';
+    }
+    cout<<endl;
 
+    numbers.merge(other_numbers);
+    copy(numbers.begin(), numbers.end(),
+         ostream_iterator<int>(cout, " "));
+    cout<<endl;
     return 0;
 }
