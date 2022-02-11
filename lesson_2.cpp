4cc25a869ed444c7fc334ac96219fd148c5b91b4
TerentevVSs <terentev.vs@phystech.edu>
2022-02-11 11:10:21 +0300

    lesson 2: RANGE(...), lambda

--- lesson_2.cpp
+++ lesson_2.cpp
@@ -1,5 +1,3 @@
-#include <list>
-#include <forward_list>
 #include <fstream>
 #include <random>
 #include <algorithm>
@@ -9,6 +7,7 @@
 #include <queue>
 #include <deque>
 #include <unordered_set>
+#include <unordered_map>
 #include <iterator>
 #include <optional>
 #include <thread>
@@ -20,23 +19,11 @@
 #include <iostream>
 using namespace std;
 
+#define RANGE(container) container.begin(), container.end()
 
 int main(){
-    list<int> numbers = {1,3,2,42 -325};
-    list<int> other_numbers = {25, -1, 30};
-    numbers.splice(next(numbers.begin()),
-                   other_numbers,
-                   other_numbers.begin(),
-                   prev(other_numbers.end()));
-    numbers.sort(less<>());
-    for(auto item: numbers){
-        cout << item << ' ';
-    }
-    cout<<endl;
-
-    numbers.merge(other_numbers);
-    copy(numbers.begin(), numbers.end(),
-         ostream_iterator<int>(cout, " "));
-    cout<<endl;
+    vector<int> nums{3,-4,5, 94,-23};
+    sort(RANGE(nums), [](auto l, auto r){return l<r;});
+    copy(RANGE(nums),ostream_iterator<int>(cout, " "));
     return 0;
 }
