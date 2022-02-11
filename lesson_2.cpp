9c9da66d246e812869599f8a9926360d68f365e7
TerentevVSs <terentev.vs@phystech.edu>
2022-02-11 09:13:10 +0300

    lesson 2 added

---
+++ lesson_2.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <random>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <iterator>
+#include <optional>
+#include <thread>
+#include <mutex>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <chrono>
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout<<'0';
+    return 0;
+}
