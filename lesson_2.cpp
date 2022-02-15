b13dee6bfc8c8010316554dcbda965c353e55e7e
TerentevVSs <terentev.vs@phystech.edu>
2022-02-15 23:11:55 +0300

    lesson 2: fix ...

--- lesson_2.cpp
+++ lesson_2.cpp
@@ -18,25 +18,26 @@
 #include <chrono>
 #include <iostream>
 #include <iomanip>
+#include <tuple>
 using namespace std;
+template<typename T, typename... Types>
+auto dynamically_construct(Types... args) {
+    return new T(args...);
+}
+struct Date {
+    Date(int year, int month, int day): year(year), month(month), day(day) {}
+    int year, month, day;
+};
 
-
+bool compare_dates(Date d1, Date d2)
+{
+    return make_tuple(d1.year, d1.month, d1.day) < make_tuple(d2.year, d2.month, d2.day);
+}
 int main(){
-    multimap<int, string> groups;
-    groups.emplace(831, "Nikita");
-    groups.insert({831, "Victor"});
-    groups.insert({831, "Alex"});
-    groups.insert({002, "Johnny"});
-    groups.insert({002, "Valery"});
-    groups.insert({001, "Pyotr"});
-
-    for(const auto& [group, student]: groups)
-        cout << setw(3) << setfill('0') <<
-        group << ": " << setw(10) << setfill(' ') << student << endl;
-    auto [left, right]= groups.equal_range(831);
-    while(left!=right){
-        cout<<left->second<<endl;
-        left++;
-    }
+    Date d1{2020, 12, 3};
+    Date d2{2020, 12, 5};
+    cout<<boolalpha<<compare_dates(d1, d2);
+    Date* date_ptr = dynamically_construct<Date>(2022, 2, 4);
+    delete date_ptr;
     return 0;
 }
