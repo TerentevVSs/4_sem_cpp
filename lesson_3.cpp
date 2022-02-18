f4b39e966aa6353bfec102f09fc6fb8d11823145
TerentevVSs <terentev.vs@phystech.edu>
2022-02-18 11:42:35 +0300

    lesson 3: nth_element

--- lesson_3.cpp
+++ lesson_3.cpp
@@ -5,7 +5,11 @@
 #include <map>
 #include <string>
 #include <numeric>
+#include <random>
+#include <chrono>
+#include <deque>
 using namespace std;
+
 
 template<typename T>
 ostream& operator<<(ostream& out, const vector<T>& data){
@@ -14,27 +18,34 @@ ostream& operator<<(ostream& out, const vector<T>& data){
     out<<*prev(data.end())<<"]";
     return out;
 }
+//template<typename T>
+//class U<T>{};
+//template<typename T, class U<T>>
+//ostream& operator<<(ostream& out, const U<T>& data){
+//    out<<"[";
+//    copy(data.begin(), prev(data.end()), ostream_iterator<T>(out, ", "));
+//    out<<*prev(data.end())<<"]";
+//    return out;
+//}
+
+template<typename T>
+ostream& operator<<(ostream& out, const deque<T>& data){
+    out<<"[";
+    copy(data.begin(), prev(data.end()), ostream_iterator<T>(out, ", "));
+    out<<*prev(data.end())<<"]";
+    return out;
+}
 
 int main(){
-//    vector<int> source = {32,623,236,76};
-//    vector<int> dest = {27,832,278,375};
-//    copy(source.begin(), source.end(), back_inserter(dest));
-//    copy(dest.begin(), dest.end(), ostream_iterator<int>(cout, " "));
-    vector<int> seq(10);
-    iota(seq.begin(), seq.end(), 66);
-    cout<<seq<<endl;
-    auto sum = accumulate(seq.begin(), seq.end(), 66);
-    auto product = accumulate(seq.begin(), seq.end(),
-                              1ll, [](auto lhs, auto rhs){return lhs*rhs;});
-    cout<<sum<<endl;
-    cout<<product<<endl;
-    vector<int> reversed_seq;
-    reverse_copy(seq.begin(), seq.end(), back_inserter(reversed_seq));
-    cout<<reversed_seq<<endl;
-    cout<<inner_product(seq.begin(), seq.end(), reversed_seq.begin(), 0)<<endl;
-    vector<int> transformed;
-    transform(seq.begin(), seq.end(), seq.begin(), back_inserter(transformed),
-              [](auto data1, auto data2){return data1*data2;});
-    cout<<accumulate(transformed.begin(), transformed.end(), 0);
+    auto seed = chrono::steady_clock::now().time_since_epoch().count();
+    auto gen = mt19937(seed);
+    auto distributon = uniform_int_distribution(1, 10);
+    deque<int> rnd;
+    generate_n(front_inserter(rnd), 10,
+               [&gen, &distributon](){return distributon(gen);});
+    cout<<rnd<<endl;
+    auto nth_iter = next(rnd.begin(), 3);
+    nth_element(rnd.begin(), nth_iter, rnd.end(), [](auto l, auto r){return l>r;});
+    cout<<rnd<<endl;
     return 0;
 }
