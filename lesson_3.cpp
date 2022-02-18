62aa8f43f66b6b7359d75b0291f68b3e66f259e7
TerentevVSs <terentev.vs@phystech.edu>
2022-02-18 11:54:45 +0300

    lesson 3: unique, erase, copy_if

--- lesson_3.cpp
+++ lesson_3.cpp
@@ -18,15 +18,6 @@ ostream& operator<<(ostream& out, const vector<T>& data){
     out<<*prev(data.end())<<"]";
     return out;
 }
-//template<typename T>
-//class U<T>{};
-//template<typename T, class U<T>>
-//ostream& operator<<(ostream& out, const U<T>& data){
-//    out<<"[";
-//    copy(data.begin(), prev(data.end()), ostream_iterator<T>(out, ", "));
-//    out<<*prev(data.end())<<"]";
-//    return out;
-//}
 
 template<typename T>
 ostream& operator<<(ostream& out, const deque<T>& data){
@@ -36,16 +27,15 @@ ostream& operator<<(ostream& out, const deque<T>& data){
     return out;
 }
 
+#define range(cont) begin(cont), end(cont)
 int main(){
-    auto seed = chrono::steady_clock::now().time_since_epoch().count();
-    auto gen = mt19937(seed);
-    auto distributon = uniform_int_distribution(1, 10);
-    deque<int> rnd;
-    generate_n(front_inserter(rnd), 10,
-               [&gen, &distributon](){return distributon(gen);});
-    cout<<rnd<<endl;
-    auto nth_iter = next(rnd.begin(), 3);
-    nth_element(rnd.begin(), nth_iter, rnd.end(), [](auto l, auto r){return l>r;});
-    cout<<rnd<<endl;
+    vector<int> numbers = {1,1,4,2,4,6,3,1,5,3};
+    cout<<numbers<<endl;
+    auto to_delete = unique(numbers.begin(), numbers.end());
+    numbers.erase(to_delete, numbers.end());
+    cout<<numbers<<endl;
+    vector<int> copied;
+    copy_if(range(numbers), back_inserter(copied), [](auto elem){return elem>4;});
+    cout<<copied;
     return 0;
 }
