254e1d8ae9dbcf5d39af59b70978a117c1310c0a
TerentevVSs <terentev.vs@phystech.edu>
2022-02-21 15:49:22 +0300

    hw_3: init

---
+++ ex_1_20.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <cmath>
+#include <map>
+#include <string>
+#include <numeric>
+#include <random>
+#include <chrono>
+#include <deque>
+using namespace std;
+
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& data){
+    out<<"[";
+    copy(data.begin(), prev(data.end()), ostream_iterator<T>(out, ", "));
+    out<<*prev(data.end())<<"]";
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const deque<T>& data){
+    out<<"[";
+    copy(data.begin(), prev(data.end()), ostream_iterator<T>(out, ", "));
+    out<<*prev(data.end())<<"]";
+    return out;
+}
+
+#define range(cont) begin(cont), end(cont)
+int main(){
+    vector<int> numbers = {1,1,4,2,4,6,3,1,5,3};
+    cout<<numbers<<endl;
+    auto to_delete = unique(numbers.begin(), numbers.end());
+    numbers.erase(to_delete, numbers.end());
+    cout<<numbers<<endl;
+    vector<int> copied;
+    copy_if(range(numbers), back_inserter(copied), [](auto elem){return elem>4;});
+    cout<<copied;
+    return 0;
+}
