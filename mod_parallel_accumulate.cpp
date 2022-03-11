3f9afe1418e5e000c9b36d51f763995107b15e04
TerentevVSs <terentev.vs@phystech.edu>
2022-03-11 18:35:20 +0300

    hw_5: task_1: refactoring

--- mod_parallel_accumulate.cpp
+++ mod_parallel_accumulate.cpp
@@ -14,7 +14,8 @@ void accumulate_block(Iterator first, Iterator last, T init, T &result) {
 }
 
 template<typename Iterator, typename T>
-T parallel_accumulate(Iterator first, Iterator last, T init, unsigned int num_workers=thread::hardware_concurrency()){
+T
+parallel_accumulate(Iterator first, Iterator last, T init, unsigned int num_workers = thread::hardware_concurrency()) {
     // 1. Проверили длину
     auto length = distance(first, last);
     if (length < 4 * num_workers) {
@@ -49,24 +50,22 @@ int main() {
     vector<long long> test_sequence(100000u);
     iota(test_sequence.begin(), test_sequence.end(), 0);
     auto timer = Timer<chrono::microseconds>();
-    int n=100;
+    int n = 100;
     vector<int> answers(100);
-    for(int j=0; j<n; j++){
-        for(int i=1; i<=100; i++)
-        {
+    for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= 100; i++) {
             timer.reset();
             timer.start();
-            auto result =
-                    parallel_accumulate(begin(test_sequence),
+            parallel_accumulate(begin(test_sequence),
                                         end(test_sequence), 0, i);
             timer.stop();
-            answers[i-1]+=timer.get_time();
+            answers[i - 1] += timer.get_time();
         }
-        cout<<j<<endl;
+        cout << j << endl;
     }
     ofstream out;
     out.open("../data.txt");
-    for_each(answers.begin(), answers.end(), [&out, &n](auto data){out<<data/n<<endl;});
+    for_each(answers.begin(), answers.end(), [&out, &n](auto data) { out << data / n << endl; });
     out.close();
 
     return 0;
