8d73d961fabe4fda5cd2dc408c8494b31c0ae3d7
TerentevVSs <terentev.vs@phystech.edu>
2022-03-11 17:11:00 +0300

    lesson 5: fix: parallel accumulate

--- lesson_5.cpp
+++ lesson_5.cpp
@@ -1,51 +1,52 @@
-#include <iostream>
 #include <thread>
-#include <vector>
 #include <numeric>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 template<typename Iterator, typename T>
-T parallel_accumulate(Iterator first, Iterator last, T init);
-
-template<typename Iterator, typename T>
 void accumulate_block(Iterator first, Iterator last, T init, T &result) {
-    result = parallel_accumulate(first, last, result);
+    result = accumulate(first, last, init);
 }
 
 template<typename Iterator, typename T>
 T parallel_accumulate(Iterator first, Iterator last, T init) {
+    // 1. Проверили длину
     auto length = distance(first, last);
-    if (length < thread::hardware_concurrency() * 4) {
-        return parallel_accumulate(first, last, init);
+    if (length < 32) {
+        return accumulate(first, last, init);
     }
+    // 2. Длина достаточна, распараллеливаем
     auto num_workers = thread::hardware_concurrency();
+    // Вычислили длину для одного потока
     auto length_per_thread = (length + num_workers - 1) / num_workers;
+    // Векторы с потоками и результатами
     vector<thread> threads;
     vector<T> results(num_workers - 1);
+    // 3. Распределяем данные (концепция полуинтервалов!)
     for (auto i = 0u; i < num_workers - 1; i++) {
         auto beginning = next(first, i * length_per_thread);
         auto ending = next(first, (i + 1) * length_per_thread);
-        threads.push_back(
-                thread(
-                        accumulate_block<Iterator, T>,
-                        beginning, ending, 0, ref(results[i])));
+        // 4. Запускаем исполнителей
+        threads.push_back(thread(
+                accumulate_block<Iterator, T>,
+                beginning, ending, 0, ref(results[i])));
     }
-    auto main_result = parallel_accumulate(next(first,
-                                                (num_workers - 1) * length_per_thread),
-                                           last, init);
-    for (auto &thread: threads) {
-        thread.join();
-    }
-    return parallel_accumulate(begin(results), end(results), main_result);
+    // Остаток данных -- в родительском потоке
+    auto main_result = accumulate(next(first, (num_workers - 1) * length_per_thread),
+                                  last, init);
+    // std::mem_fun_ref -- для оборачивания join().
+    for_each(begin(threads), end(threads), mem_fun_ref(&thread::join));
+    // 5. Собираем результаты
+    return accumulate(begin(results), end(results), main_result);
 }
 
 int main() {
     vector<int> test_sequence(100u);
     iota(test_sequence.begin(), test_sequence.end(), 0);
-    auto result = parallel_accumulate<decltype(
-            begin(test_sequence))>(begin(test_sequence),
-                                   end(test_sequence), 0);
-    cout<<result;
-    return 0;
+    auto result =
+            parallel_accumulate(begin(test_sequence),
+                                end(test_sequence), 0);
+    cout << "Result is " << result << endl;
 }
