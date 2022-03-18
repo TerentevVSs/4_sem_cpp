de70e82e4859e20e01adbff49014ca7ccab652aa
TerentevVSs <terentev.vs@phystech.edu>
2022-03-18 11:16:11 +0300

    lesson_6: lock_quard

--- lessons/lesson_6.cpp
+++ lessons/lesson_6.cpp
@@ -10,43 +10,29 @@ using namespace std;
 
 mutex lock_cout;
 mutex other_lock;
-
-void function_for_thread(const string &word) {
-    lock_cout.lock();
-    this_thread::sleep_for(chrono::milliseconds(1));
-    other_lock.lock();
-    cout << "The " << "word " << "is " << word << "!" << endl;
-    lock_cout.unlock();
-    other_lock.unlock();
-}
-
-void function_for_main(const string &word) {
-    other_lock.lock();
-    this_thread::sleep_for(chrono::milliseconds(1));
-    lock_cout.lock();
-    cout << "The " << "word " << "is " << word << "!" << endl;
-    other_lock.unlock();
-    lock_cout.unlock();
-}
-
-int some_variable = 0;
-atomic<int> atomic_variable = 0;
-
-void function() {
-    for (auto i = 0; i < 300000; i++) {
-        atomic_variable += 1;
+template<typename Mutex>
+class RAIILockWrapper{
+public:
+    RAIILockWrapper(Mutex& given): m(given){
+        m.lock();
     }
-}
+    ~RAIILockWrapper(){
+        m.unlock();
+    }
+private:
+    Mutex& m;
+};
+mutex m;
+void print_word(const string& word) {
+    lock_guard wrapper(m);
+    cout<< "ABC "<<"DEF "<<word<<" s else ";
+};
+
 
 int main() {
-//    string word = "DGAP";
-//    lock(lock_cout, other_lock);
-//    thread other(function_for_thread, "DGAP");
-//    function_for_main("LPR");
-//    other.join();
-    thread other(function);
-    function();
+    string word = "DGAP";
+    thread other(print_word, "DGAP");
+    print_word("LPR");
     other.join();
-    cout << atomic_variable;
     return 0;
 }
